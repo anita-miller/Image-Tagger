49ff8a5088c4f596e2231115fde7a50b691e64c2
anita nseri <a.naseri@student.unimelb.edu.au>
2019-04-12 22:15:39 +1000

    taking the username and take user to next page, and change the name of file of htmls

--- image-tagger.c
+++ image-tagger.c
@@ -52,6 +52,123 @@ static bool manage_http_request(int sockfd)
             printf("socket %d close the connection\n", sockfd);
         return false;
     }
+
+
+    // terminate the string
+    buff[n] = 0;
+
+    char * curr = buff;
+
+    // parse the method
+    METHOD method = UNKNOWN;
+    if (strncmp(curr, "GET ", 4) == 0)
+    {
+        curr += 4;
+        method = GET;
+    }
+    else if (strncmp(curr, "POST ", 5) == 0)
+    {
+        curr += 5;
+        method = POST;
+    }
+    else if (write(sockfd, HTTP_400, HTTP_400_LENGTH) < 0)
+    {
+        perror("write");
+        return false;
+    }
+
+    // sanitise the URI
+    while (*curr == '.' || *curr == '/')
+        ++curr;
+    // assume the only valid request URI is "/" but it can be modified to accept more files
+    if (*curr == ' ')
+        if (method == GET)
+        {
+            // get the size of the file
+            struct stat st;
+            stat("html/1_intro.html", &st);
+            n = sprintf(buff, HTTP_200_FORMAT, st.st_size);
+            // send the header first
+            if (write(sockfd, buff, n) < 0)
+            {
+                perror("write");
+                return false;
+            }
+            // send the file
+            int filefd = open("html/1_intro.html", O_RDONLY);
+            do
+            {
+                n = sendfile(sockfd, filefd, NULL, 2048);
+            }
+            while (n > 0);
+            if (n < 0)
+            {
+                perror("sendfile");
+                close(filefd);
+                return false;
+            }
+            close(filefd);
+        }
+        else if (method == POST)
+        {
+            // locate the username, it is safe to do so in this sample code, but usually the result is expected to be
+            // copied to another buffer using strcpy or strncpy to ensure that it will not be overwritten.
+            char temp[2049];
+            char * username = strstr(buff, "user=") + 5;
+            int username_length = strlen(username);
+            // the length needs to include the ", " before the username
+            long added_length = username_length + 2;
+
+            // get the size of the file
+            struct stat st;
+            stat("html/1_intro.html", &st);
+            // increase file size to accommodate the username
+            long size = st.st_size + added_length;
+            n = sprintf(buff, HTTP_200_FORMAT, size);
+            // send the header first
+            if (write(sockfd, buff, n) < 0)
+            {
+                perror("write");
+                return false;
+            }
+            // read the content of the HTML file
+            int filefd = open("html/1_intro.html", O_RDONLY);
+            n = read(filefd, buff, 2048);
+            if (n < 0)
+            {
+                perror("read");
+                close(filefd);
+                return false;
+            }
+            close(filefd);
+            // move the trailing part backward
+            int p1, p2;
+            for (p1 = size - 1, p2 = p1 - added_length; p1 >= size - 25; --p1, --p2)
+                buff[p1] = buff[p2];
+            ++p2;
+            // put the separator
+            buff[p2++] = ',';
+            buff[p2++] = ' ';
+            // copy the username
+            strncpy(buff + p2, username, username_length);
+            if (write(sockfd, buff, size) < 0)
+            {
+                perror("write");
+                return false;
+            }
+        }
+        else
+            // never used, just for completeness
+            fprintf(stderr, "no other methods supported");
+    // send 404
+    else if (write(sockfd, HTTP_404, HTTP_404_LENGTH) < 0)
+    {
+        perror("write");
+        return false;
+    }
+
+    return true;
+
 }
 
 int main(int argc, char * argv[])
@@ -144,7 +261,7 @@ int main(int argc, char * argv[])
                     }
                 }
                 // a request is sent from the client
-                else if (!handle_http_request(i))
+                else if (!manage_http_request(i))
                 {
                     close(i);
                     FD_CLR(i, &masterfds);
