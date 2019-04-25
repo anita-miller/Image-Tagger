418d2e8f89d50b7d2f7c8979bd3e0a4589c2fb70
anita nseri <a.naseri@student.unimelb.edu.au>
2019-04-25 19:30:51 +1000

    add comments

--- helper_functions.h
+++ helper_functions.h
@@ -32,21 +32,28 @@ METHOD parseMethod(char *curr, METHOD method);
 //parsing which html type to load from buffer
 Page parseCorrectHtml(char *temp, Page page);
 
+//add the username to html file dynamically
 bool addUserName(int sockfd, char *buff, char *username, long size);
+
+// set up the user's value thats playing to sockfd
 void setup_up_users(int sockfd);
+
+//reset users to -1 when game is quited
 void reset_users(int sockfd);
 
 void loadPOSTHtml(int n, int sockfd, char *buff, const char *pathname);
+
 void loadGETHtml(int n, int sockfd, char *buff, const char *pathname);
 
+// check if the guess made by one player exists in the list of guesses made by other player
 void check_if_gusses_match(char *keyword, char user2_guesses[100][100], int number_guesses_user2, char user1_guesses[100][100], int number_guesses_user1);
 
 int load_quit_page(int n, int sockfd,char *buff);
+
 int load_start_page(int n, int sockfd, char *buff);
+
 int load_accepted_page(int n, int sockfd, char *buff);
 
 void manage_GET_requests(int n, int sockfd, char *buff, Page page);
 
 bool manage_POST_requests(int n, int sockfd, char *buff, Page page);
-
-//static bool manage_http_request(int sockfd);
